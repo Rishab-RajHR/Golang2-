7421aa9971b6a690b2063709ed8a64a34550f272
Rishab Raj <rr0948555@gmail.com>
2026-04-18 11:16:41 +0530

    Else-if

--- if-else/main.go
+++ if-else/main.go
@@ -16,10 +16,17 @@ func main() {
 	z := 10
 	if z > 10 {
 		fmt.Println("z is greater than 10")
-	} else if z < 5 {
+	} else if z > 5 {
 		fmt.Println("z is greater than 5 but smaller than 10")
 	} else {
 		fmt.Println("z is smaller than 5")
+	}
+
+	y := 10
+	if x > 5 && (y > 5 || z < 43) {
+		fmt.Println("Hey How are you ?")
+	} else {
+		fmt.Println("Learn Programming with Hello World")
 	}
 
 }
