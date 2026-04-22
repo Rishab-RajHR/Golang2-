517349d6b3df0b55a1df77a808e9f6686078ebd8
Rishab Raj <rr0948555@gmail.com>
2026-04-22 07:54:09 +0530

    Pointer;

--- Pointer/main.go
+++ Pointer/main.go
@@ -12,5 +12,10 @@ func main() {
 
 	// fmt.Println("Num has value: ", num)
 	fmt.Println("Pointer contains: ", ptr)
-	fmt.Println("Data contains through Pointer: ", *ptr)
+	fmt.Println("Data contains through Pointer: ", *ptr) // 2
+
+	var pointer *int // default pointer == nil
+	if pointer == nil {
+		fmt.Println("Pointer is not assigned")
+	}
 }
