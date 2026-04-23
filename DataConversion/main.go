58ec027db6e46f5a5809a7eb724d13fe05fd3f9c
Rishab Raj <rr0948555@gmail.com>
2026-04-23 11:18:27 +0530

    Data

--- DataConversion/main.go
+++ DataConversion/main.go
@@ -6,4 +6,8 @@ func main() {
 	var num int = 42
 	fmt.Println("Number is ", num)
 	fmt.Printf("Type of num is %T\n", num)
+
+	var data float64 = float64(num)
+	fmt.Println("Data is ", data)
+	fmt.Printf("Type of Data is %T\n", data)
 }
