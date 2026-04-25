dfa2ed8c7f2ff63677796e00129129c28065de81
Rishab Raj <rr0948555@gmail.com>
2026-04-25 10:54:23 +0530

    Strings

--- Strings/main.go
+++ Strings/main.go
@@ -9,4 +9,8 @@ func main() {
 	data := "apple,orange,banana"
 	parts := strings.Split(data, ",")
 	fmt.Println(parts)
+
+	str := "one two three four two two five"
+	count := strings.Count(str, "two")
+	fmt.Println("count: ", count)
 }
