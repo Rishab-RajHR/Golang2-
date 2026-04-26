16ff9468de7f92ef335bc75d265ae0929b2b520a
Rishab Raj <rr0948555@gmail.com>
2026-04-26 10:57:02 +0530

    Time

--- Time/main.go
+++ Time/main.go
@@ -14,8 +14,8 @@ func main() {
 	formatted := currentTime.Format("2006-02-04, 3:04 PM")
 	fmt.Println("Formatted time: ", formatted)
 
-	layout_str := "2006-01-02"
-	dateStr := "2023-11-25"
+	layout_str := "2006/01/02"
+	dateStr := "2023/11/25"
 	formatted_time, _ := time.Parse(layout_str, dateStr)
 	fmt.Println("Formatted time:", formatted_time)
 }
