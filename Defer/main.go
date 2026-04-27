d855e77e5892d67cb423d28c0772a55a8e9de1cf
Rishab Raj <rr0948555@gmail.com>
2026-04-27 10:13:29 +0530

    Defer

--- Defer/main.go
+++ Defer/main.go
@@ -9,7 +9,9 @@ func add(a, b int) int {
 func main() {
 	fmt.Println("Starting of the Program")
 	data := add(5, 6)
-	fmt.Println("Data is : ", data)
-	fmt.Println("Middle of the Program")
+
+	// When we use Differ then it uses Stack(LIFO)
+	defer fmt.Println("Data is : ", data)
+	defer fmt.Println("Middle of the Program")
 	fmt.Println("End of the Program")
 }
