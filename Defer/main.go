ae0d3a7331ff12759f2566163414d20c06582b61
Rishab Raj <rr0948555@gmail.com>
2026-04-27 10:11:34 +0530

    Defer

--- Defer/main.go
+++ Defer/main.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+func add(a, b int) int {
+	return a + b
+}
+
 func main() {
 	fmt.Println("Starting of the Program")
+	data := add(5, 6)
+	fmt.Println("Data is : ", data)
 	fmt.Println("Middle of the Program")
 	fmt.Println("End of the Program")
 }
