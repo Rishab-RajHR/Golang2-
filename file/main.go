a268b2311891f5fa36ada76943c3bb413ad13f6e
Rishab Raj <rr0948555@gmail.com>
2026-04-28 10:42:16 +0530

    File

--- file/main.go
+++ file/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	file, err := os.Create("exmaple.txt")
+	file, err := os.Create("example.txt")
 	if err != nil {
 		fmt.Println("Error while creating file: ", err)
 		return
 	}
 	defer file.Close()
+
+	content := "Hello World By Alex"
+	_, errors := io.WriteString(file, content)
+	if errors != nil {
+		fmt.Println("Error while writing file: ", errors)
+		return
+	}
+
+	fmt.Println("Successfully Created File")
 }
