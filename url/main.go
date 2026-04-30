5fc98acab1528fcefd408f5199e02eb7ad016d96
Rishab Raj <rr0948555@gmail.com>
2026-04-30 10:45:22 +0530

    url

--- url/main.go
+++ url/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+)
 
 func main() {
 	fmt.Println("Learning URL")
-	url := "http://localhost:5173/"
-	fmt.Printf("Type of URL: %T\n", url)
+	myURL := "http://localhost:5173/"
+	fmt.Printf("Type of URL: %T\n", myURL)
+
+	parsedURL, err := url.Parse(myURL)
+	if err != nil {
+		fmt.Println("Can't parse URL ", err)
+		return
+	}
+	fmt.Printf("Type of URL: %T\n", parsedURL)
 }
