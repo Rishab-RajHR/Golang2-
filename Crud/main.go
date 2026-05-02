593b6db4518eb43c632d5fc1acd111af721debf8
Rishab Raj <rr0948555@gmail.com>
2026-05-02 10:31:45 +0530

    Crud

--- Crud/main.go
+++ Crud/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Learning CRUD...")
+	res, err := http.Get("https://jsonplaceholder.typicode.com/todos")
+	if err != nil {
+		fmt.Println("Error getting : ", err)
+		return
+	}
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error reading : ", err)
+		return
+	}
+	fmt.Println("Data: ", string(data))
+
 }
