3ba7f66d2235ace74ee62a6dddee484eaf7493c6
Rishab Raj <rr0948555@gmail.com>
2026-05-02 19:52:06 +0530

    POST

--- crudpost/main.go
+++ crudpost/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 type Todo struct {
@@ -44,8 +45,42 @@ func performGetRequest() {
 	fmt.Println("completed response: ", todos)
 }
 
+func performPostRequest() {
+	todo := Todo{
+		UserID:    23,
+		Title:     "Alex Pandian",
+		Completed: true,
+	}
+
+	// Convert the Todo struct to JSON
+	jsonData, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("Error marshalling : ", err)
+		return
+	}
+
+	// Convert json data to string
+	jsonReader := strings.NewReader(string(jsonData))
+
+	myURL := "https://jsonplaceholder.typicode.com/todos"
+
+	// send POST request
+	res, err := http.Post(myURL, "application/json", jsonReader)
+	if err != nil {
+		fmt.Println("Error sending request : ", err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	// data, _ := ioutil.ReadAll(res.Body)
+	// fmt.Println("Response : ", string(data))
+
+	fmt.Println("Response status : ", res.Status)
+}
+
 func main() {
 	fmt.Println("Learning CRUD...")
-	performGetRequest()
-
+	// performGetRequest()
+	performPostRequest()
 }
