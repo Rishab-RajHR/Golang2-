0ab65bb2159a598d6e6c7b98e8c11a551e71388a
Rishab Raj <rr0948555@gmail.com>
2026-05-02 10:22:49 +0530

    Golang

--- Defer/main.go
+++ Defer/main.go
@@ -11,6 +11,7 @@ func main() {
 	data := add(5, 6)
 
 	// When we use Differ then it uses Stack(LIFO)
+	// Defer Keyword is used in function call to execute that function at the last
 	defer fmt.Println("Data is : ", data)
 	defer fmt.Println("Middle of the Program")
 	fmt.Println("End of the Program")
