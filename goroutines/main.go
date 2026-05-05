15f352cbb96ac3d1e4de62bc5a1ab181f4fb0414
Rishab Raj <rr0948555@gmail.com>
2026-05-05 22:41:52 +0530

    Go Routine

--- goroutines/main.go
+++ goroutines/main.go
@@ -7,7 +7,7 @@ import (
 
 func sayHello() {
 	fmt.Println("Hello, World!")
-	time.Sleep(100 * time.Millisecond) // Simulating some work
+	time.Sleep(2000 * time.Millisecond) // Simulating some work
 	fmt.Println("sayHello function ended successfully")
 }
 
@@ -19,4 +19,7 @@ func main() {
 	fmt.Println("Learning goroutines")
 	sayHello()
 	sayHi()
+
+	// Wait for a moment to allow the goroutine to finish
+	time.Sleep(1000 * time.Millisecond)
 }
