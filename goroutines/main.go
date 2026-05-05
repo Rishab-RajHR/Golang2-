20326d2fc0115e6066fcab5255fbaff3ba605817
Rishab Raj <rr0948555@gmail.com>
2026-05-05 22:36:50 +0530

    go routines

--- goroutines/main.go
+++ goroutines/main.go
@@ -1,12 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func sayHello() {
 	fmt.Println("Hello, World!")
+	time.Sleep(100 * time.Millisecond) // Simulating some work
+	fmt.Println("sayHello function ended successfully")
+}
+
+func sayHi() {
+	fmt.Println("Hi Alex :)")
 }
 
 func main() {
 	fmt.Println("Learning goroutines")
 	sayHello()
+	sayHi()
 }
