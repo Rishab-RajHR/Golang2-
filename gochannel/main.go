624a5ab540c9749e2c9a2cbcb04f4614be99be34
Rishab Raj <rr0948555@gmail.com>
2026-05-11 22:34:12 +0530

    Go Channel

--- gochannel/main.go
+++ gochannel/main.go
@@ -5,7 +5,8 @@ import (
 	"sync"
 )
 
-func worker(i int) {
+func worker(i int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Printf("Worker %d started\n", i)
 	// some task is happening
 	fmt.Printf("Worker %d end\n", i)
@@ -14,11 +15,14 @@ func worker(i int) {
 func main() {
 	fmt.Println("Explore goroutine started")
 
-	var wq sync.WaitGroup
+	var wg sync.WaitGroup
 	// Start 3 worker goroutines
 	for i := 1; i <= 3; i++ {
-		wq.Add(1) // Increment the WaitGroup counter
-		go worker(i)
+		wg.Add(1) // Increment the WaitGroup counter
+		go worker(i, &wg)
 	}
+
+	// wait for all workers to finish
+	wg.Wait()
 	fmt.Println("Workers task complete")
 }
