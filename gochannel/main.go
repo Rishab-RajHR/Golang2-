6a9755d1c4b6cf221768aaceb20ee96318bcabe6
Rishab Raj <rr0948555@gmail.com>
2026-05-11 22:28:23 +0530

    Go channel

--- gochannel/main.go
+++ gochannel/main.go
@@ -1,19 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func worker(i int) {
-	fmt.Printf("Worker %d started", i)
+	fmt.Printf("Worker %d started\n", i)
 	// some task is happening
-	fmt.Printf("Worker %d end", i)
+	fmt.Printf("Worker %d end\n", i)
 }
 
 func main() {
 	fmt.Println("Explore goroutine started")
 
+	var wq sync.WaitGroup
 	// Start 3 worker goroutines
-	for i := 0; i < 3; i++ {
-		worker(i)
+	for i := 1; i <= 3; i++ {
+		wq.Add(1) // Increment the WaitGroup counter
+		go worker(i)
 	}
 	fmt.Println("Workers task complete")
 }
