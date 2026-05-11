e77d60fda2911b0bc96aa4bfef4c6616569bf35e
Rishab Raj <rr0948555@gmail.com>
2026-05-11 22:24:09 +0530

    Go Channel

--- gochannel/main.go
+++ gochannel/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func worker(i int) {
+	fmt.Printf("Worker %d started", i)
+	// some task is happening
+	fmt.Printf("Worker %d end", i)
+}
+
 func main() {
 	fmt.Println("Explore goroutine started")
+
+	// Start 3 worker goroutines
+	for i := 0; i < 3; i++ {
+		worker(i)
+	}
+	fmt.Println("Workers task complete")
 }
